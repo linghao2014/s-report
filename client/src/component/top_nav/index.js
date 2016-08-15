2e1037d144444d32b75e4ef7cd29c2cc3331da93
liwei <liwei@youzan.com>
2016-08-15 23:10:50 +0800

    侧栏导航

--- client/src/component/top_nav/index.js
+++ client/src/component/top_nav/index.js
@@ -2,7 +2,7 @@
  * 一级导航
  */
 import React from 'react';
-import {Drawer} from 'material-ui';
+import {Drawer, Menu, MenuItem, RaisedButton, Avatar} from 'material-ui';
 import {style} from './index.scss';
 
 export default React.createClass({
@@ -16,6 +16,21 @@ export default React.createClass({
                 open={this.props.forceOpen || this.props.open}
                 onRequestChange={this.props.onRequestChange}>
                 <h1>简报</h1>
+                <div className="user">
+                    <a href="#">
+                    <Avatar className="avatar" src="http://p3.music.126.net/O__ztFTUL84GOTUFLY3u7g==/1391981724404463.jpg?param=200y200"/>
+                    李大伟
+                    </a>
+                </div>
+                <Menu>
+                    <MenuItem primaryText="报告"/>
+                    <MenuItem primaryText="群组"/>
+                    <MenuItem primaryText="管理员设置"/>
+                </Menu>
+                <RaisedButton
+                    className="logout"
+                    label="退出"
+                    secondary/>
             </Drawer>
         );
     }
