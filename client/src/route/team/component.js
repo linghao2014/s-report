835622c6bdc0021986126b4a668e079644db6b01
liwei <liwei@youzan.com>
2016-08-29 00:48:14 +0800

    小组创建与删除

--- client/src/route/team/component.js
+++ client/src/route/team/component.js
@@ -2,95 +2,144 @@
  * 群组
  */
 import React from 'react';
-import {FlatButton, IconButton, ListItem, Avatar, Subheader} from 'material-ui';
+import {browserHistory} from 'react-router';
+import {FlatButton, IconButton, ListItem, Avatar, Subheader, Dialog,
+    TextField, Toggle} from 'material-ui';
 import AddIcon from 'material-ui/svg-icons/content/add';
 import DeleteIcon from 'material-ui/svg-icons/action/delete';
 import SetIcon from 'material-ui/svg-icons/action/settings';
+import _ from 'lodash';
+import {fetch} from 'lib/util';
+import popup from 'cpn/popup';
 import RespBox from 'cpn/resp_box';
 import {style} from './index.scss';
 
-module.exports =  React.createClass({
+const Teams = function (props) {
+    return (
+        <ul className="teams">
+            {
+                props.list.map(t => <li key={t.id}>
+                    <h3>{t.name}</h3>
+                    <div className="count">
+                        <span>成员:{t.memberSize}</span>
+                        <span>关注:{t.followSize}</span>
+                    </div>
+                    {
+                        t.admin &&
+                        <div className="ops">
+                            <IconButton onTouchTap={props.onDelete.bind(null, t)}>
+                                <DeleteIcon color="#666"/>
+                            </IconButton>
+                            <IconButton onTouchTap={e => browserHistory.push('team/edit')}>
+                                <SetIcon color="#666"/>
+                            </IconButton>
+                        </div>
+                    }
+                </li>)
+            }
+        </ul>
+    );
+};
+
+module.exports = React.createClass({
     getInitialState() {
-        return {};
+        return {showCreate: false};
+    },
+    componentDidMount() {
+        fetch('/api/team/myList')
+            .then(d => {
+                this.setState({
+                    myTeams: d.teams
+                });
+            });
+        fetch('/api/team/followList')
+            .then(d => {
+                this.setState({
+                    followTeams: d.teams
+                });
+            });
     },
     render() {
         let barConf = {
             title: '群组',
-            iconElementRight: <IconButton onTouchTap={this._create}><AddIcon/></IconButton>
+            iconElementRight: <IconButton onTouchTap={e => this.setState({showCreate: true})}><AddIcon/></IconButton>
         };
+        let actions = [
+            <FlatButton
+                primary
+                label="取消"
+                onTouchTap={e => this.setState({showCreate: false})}/>,
+            <FlatButton
+                disabled={!this.state.createName}
+                primary
+                label="确定"
+                onTouchTap={this._createTeam}/>
+        ];
         return (
             <RespBox className={style} barConf={barConf}>
-                <Subheader>我管理的群组</Subheader>
-                <ul className="teams">
-                    <li>
-                        <h3>超级前端组</h3>
-                        <div className="count">
-                            <span>成员:4</span>
-                            <span>关注:4</span>
-                        </div>
-                        <div className="ops">
-                            <IconButton>
-                                <DeleteIcon color="#666"/>
-                            </IconButton>
-                            <IconButton>
-                                <SetIcon color="#666"/>
-                            </IconButton>
-                        </div>
-                    </li>
-                </ul>
-                <Subheader>我加入的群组</Subheader>
-                <ul className="teams">
-                    <li>
-                        <h3>超级前端组</h3>
-                        <div className="count">
-                            <span>成员:4</span>
-                            <span>关注:4</span>
-                        </div>
-                    </li>
-                    <li>
-                        <h3>超级前端组</h3>
-                        <div className="count">
-                            <span>成员:4</span>
-                            <span>关注:4</span>
-                        </div>
-                    </li>
-                    <li>
-                        <h3>超级前端组</h3>
-                        <div className="count">
-                            <span>成员:4</span>
-                            <span>关注:4</span>
-                        </div>
-                    </li>
-                </ul>
-                <Subheader>我关注的群组</Subheader>
-                <ul className="teams">
-                    <li>
-                        <h3>超级前端组</h3>
-                        <div className="count">
-                            <span>成员:4</span>
-                            <span>关注:4</span>
-                        </div>
-                        <FlatButton
-                            secondary
-                            className="follow"
-                            label="取消关注"/>
-                    </li>
-                </ul>
-                <Subheader>未关注的群组</Subheader>
-                <ul className="teams">
-                    <li>
-                        <h3>超级前端组</h3>
-                        <div className="count">
-                            <span>成员:4</span>
-                            <span>关注:4</span>
-                        </div>
-                        <FlatButton
-                            primary
-                            className="follow"
-                            label="关注"/>
-                    </li>
-                </ul>
+                {
+                    this.state.myTeams && this.state.myTeams.length &&
+                    [
+                        <Subheader key="0">我所在的小组</Subheader>,
+                        <Teams key="1" list={this.state.myTeams} onDelete={this._deleteTeam}/>
+                    ]
+                }
+                <Subheader key="0">全部小组 &gt;&gt;</Subheader>
+                <Dialog
+                    contentStyle={{maxWidth: '450px'}}
+                    title="新建小组"
+                    actions={actions}
+                    open={this.state.showCreate}>
+                    <TextField
+                        hintText="组名"
+                        value={this.state.createName || ''}
+                        onChange={e => this.setState({createName: e.target.value})}
+                        fullWidth={true}/>
+                    <Toggle
+                        toggle={!!this.state.createCanBeFollow}
+                        onToggle={e => this.setState({createCanBeFollow: e.target.checked})}
+                        style={{marginTop: '10px'}}
+                        labelPosition="right"
+                        label="允许关注"/>
+                </Dialog>
             </RespBox>
         );
+    },
+    _createTeam() {
+        fetch('/api/team/create', {
+            method: 'post',
+            body: {
+                name: this.state.createName,
+                canBeFollow: this.state.createCanBeFollow
+            }
+        })
+            .then(d => {
+                this.setState({
+                    showCreate: false,
+                    createCanBeFollow: false,
+                    createName: false
+                });
+                popup.success('创建成功');
+                browserHistory.push('team/edit');
+            })
+            .catch(e => {
+                popup.error(e.msg || '小组创建失败');
+            });
+    },
+    _deleteTeam(t) {
+        popup.confirm({
+            msg: '确定删除小组?',
+            onOk: () => {
+                fetch('/api/team/delete?teamId=' + t.id)
+                    .then(d => {
+                        _.remove(this.state.myTeams, {id: t.id});
+                        this.forceUpdate();
+                        popup.success('删除成功');
+                    })
+                    .catch(e => {
+                        popup.success('删除失败');
+                    })
+            }
+        });
     }
 });
